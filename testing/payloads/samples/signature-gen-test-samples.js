bdf03b47afd0bf7bcf37050629f5acaeb03cf604
akalanka <akalankapathirage22@gmail.com>
2026-03-04 19:40:01 +0530

    fixed the issue with nonce starting from 1

--- testing/payloads/samples/signature-gen-test-samples.js
+++ testing/payloads/samples/signature-gen-test-samples.js
@@ -4,20 +4,20 @@ export const sigGenSamples = [
     user: "0x9F77cBDb561aaD32b403695306e3eea53F9B40e7",
     merchant: "0x9F77cBDb561aaD32b403695306e3eea53F9B40e7", // TODO: i have used my main dev wallet as both user and merchant here for testing. If needed update the addresses or logic in contract to support admin functions.
     amount: 1000,
-    nonce: 1,
+    nonce: 0,
   },
   {
     name: "Basic Authorization",
     user: "0x9F77cBDb561aaD32b403695306e3eea53F9B40e7", // Corresponds to the test private key
     merchant: "0x9F77cBDb561aaD32b403695306e3eea53F9B40e7", // TODO: i have used my main dev wallet as both user and merchant here for testing. If needed update the addresses or logic in contract to support admin functions.
     amount: 100,
-    nonce: 2,
+    nonce: 1,
   },
   {
     name: "Different Merchant",
     user: "0x9F77cBDb561aaD32b403695306e3eea53F9B40e7",
     merchant: "0x1111111111111111111111111111111111111111",
     amount: 50,
-    nonce: 3,
+    nonce: 2,
   },
 ];
